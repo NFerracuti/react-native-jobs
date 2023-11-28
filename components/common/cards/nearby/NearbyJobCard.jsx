1180cc1587dca93e79ee2ca9ad8fa7b06012e0b0
NFerracuti <npferracuti@gmail.com>
2023-11-27 22:39:41 -0500

    nearbyjob formatting

--- components/common/cards/nearby/NearbyJobCard.jsx
+++ components/common/cards/nearby/NearbyJobCard.jsx
@@ -1,13 +1,34 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, Image } from 'react-native'
 
-import styles from './nearbyjobcard.style'
+import styles from "./nearbyjobcard.style";
+import { checkImageURL } from "../../../../utils";
 
-const NearbyJobCard = () => {
+const NearbyJobCard = (job, handleNavigate) => {
+
+  // console.log('data job: ', job.job);
   return (
-    <View>
-      <Text>NearbyJobCard</Text>
-    </View>
+    <TouchableOpacity
+      style={styles.container}
+      onPress={() => handleNavigate()}>
+      <TouchableOpacity style={styles.logoContainer}>
+        <Image
+          source={{
+            uri: checkImageURL(job.job.employer_logo)
+              ? job.job.employer_logo
+              : "https://i.imgur.com/sJ6MrIV.png"
+          }}
+          resizeMode="contain"
+          style={styles.logoImage}
+        />
+      </TouchableOpacity>
+
+      <View style={styles.textContainer}>
+        <Text style={styles.jobName} numberOfLines={1}>
+          {job.job.job_title}
+        </Text>
+        <Text style={styles.location}>{job.job.job_country}</Text>
+      </View>
+    </TouchableOpacity>
   )
 }
 
